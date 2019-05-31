afb4f5052cf4ff3613f15affcefe6d7120f7d13c
hackeryard <hrunker@gmail.com>
2019-05-31 11:32:04 +0800

    add query_string rewrite

--- public/app/plugins/datasource/prometheus/datasource.ts
+++ public/app/plugins/datasource/prometheus/datasource.ts
@@ -277,8 +277,18 @@ export class PrometheusDatasource {
     }
 
     const url = '/api/v1/query_range';
+
+    console.log("++debug: ", query.expr);
+    var last_query = query.expr;
+    // rate(ifHCInOctets{instance=~"10.10.28.1",ifAliasTest="Vlanif28#1F-north-vlan",ifAdminStatus="1"}[10m])*8
+    // last_query = 'rate(ifHCInOctets{instance=~"10.10.28.1",ifAliasTest="Vlanif28#1F-north-vlan",ifAdminStatus="1"}[10m])*8';
+    //'rate(ifHCOutOctets{instance=~"$ip",ifAliasTest=~"$Interface",ifAdminStatus="1",ifOperStatus="1"}[10m])*8'
+    last_query = last_query.split("#")[0] + '",' +  last_query.split("#")[1].split(',').slice(1);
+    //"rate(ifHCInOctets{instance=~"10.10.28.1",ifAliasTest="Vlanif28",ifAdminStatus="1"}[10m])*8"
+    console.log("++debug: ", last_query);
+    
     const data = {
-      query: query.expr,
+      query: last_query,
       start: start,
       end: end,
       step: query.step,
